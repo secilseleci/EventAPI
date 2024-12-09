224df1507da702769481475d2bc3868e156ec3d9
Seçil Seleci <143125124+secilseleci@users.noreply.github.com>
2024-12-10 02:42:28 +0300

    ignore files

--- src/Infrastructer/ExtensionMethods/ConfigurationBuilderExtensionMethods.cs
+++ src/Infrastructer/ExtensionMethods/ConfigurationBuilderExtensionMethods.cs
@@ -10,29 +10,7 @@ namespace Infrastructure.ExtensionMethods
     public static class ConfigurationBuilderExtensionMethods
     {
         public static IConfigurationBuilder AddAppSettings(
-        this IConfigurationBuilder configurationBuilder, string? environmentName = null, bool setBasePath = true)
-        {
-            if (setBasePath)
-            {
-                configurationBuilder
-                    .SetBasePath(AppContext.BaseDirectory);
-            }
-
-            configurationBuilder.AddEnvironmentVariables()
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-
-            if (string.IsNullOrEmpty(environmentName))
-            {
-                environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            }
-
-            if (!string.IsNullOrEmpty(environmentName))
-            {
-                configurationBuilder
-                    .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true);
-            }
-
-            return configurationBuilder;
+      
         }
     }
 }
