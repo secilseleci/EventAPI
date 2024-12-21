b03428d08011e31fd1989025f7a1e58c5ff9580a
Seçil Seleci <143125124+secilseleci@users.noreply.github.com>
2024-12-21 15:07:47 +0300

    Update ApplicationBuilderExtensionMethods.cs

--- src/Infrastructer/ExtensionMethods/ApplicationBuilderExtensionMethods.cs
+++ src/Infrastructer/ExtensionMethods/ApplicationBuilderExtensionMethods.cs
@@ -8,12 +8,7 @@ namespace Infrastructure.ExtensionMethods
 {
     public static class ApplicationBuilderExtensionMethods
     {
-        public static async Task EnsureDatabaseCreated(this IApplicationBuilder app, CancellationToken cancellationToken = default)
-        {
-            using var scope = app.ApplicationServices.CreateScope();
-            var context = scope.ServiceProvider.GetRequiredService<EventApiDbContext>();
-            await context.Database.EnsureCreatedAsync(cancellationToken);
-        }
+       //private
     }
 
 }
