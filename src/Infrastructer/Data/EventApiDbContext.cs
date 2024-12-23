5811f35eb42c36747e075bcfb8588d48f95acac1
Seçil Seleci <143125124+secilseleci@users.noreply.github.com>
2024-12-23 07:08:19 +0300

    Update EventApiDbContext.cs

--- src/Infrastructer/Data/EventApiDbContext.cs
+++ src/Infrastructer/Data/EventApiDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Core.Entities;
+using Core.Entities;
 using Infrastructure.Data.Seeders;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,39 +10,53 @@ namespace Infrastructure.Data
         {
             modelBuilder.SeedUsers();
             modelBuilder.SeedEvents();
-           
             modelBuilder.SeedParticipants();
             modelBuilder.SeedInvitations();
+
             base.OnModelCreating(modelBuilder);
 
+            // User -> Event (Cascade delete: User silinince Event'ler de silinir)
             modelBuilder.Entity<User>()
-            .HasMany(user => user.OrganizedEvents)
-            .WithOne(evt => evt.Organizer)
-            .HasForeignKey(evt => evt.OrganizerId)
-             .OnDelete(DeleteBehavior.Cascade);
+                .HasMany(user => user.OrganizedEvents)
+                .WithOne(evt => evt.Organizer)
+                .HasForeignKey(evt => evt.OrganizerId)
+                .OnDelete(DeleteBehavior.Cascade);
 
+            modelBuilder.Entity<User>()
+                .HasMany(user => user.SentInvitations)
+                .WithOne(inv => inv.Organizer)
+                .HasForeignKey(inv => inv.OrganizerId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+
+            // User -> Participant (Cascade delete: User silinince Participant kayıtları da silinir)
+            modelBuilder.Entity<User>()
+                .HasMany(user => user.ParticipatedEvents)
+                .WithOne(part => part.User)
+                .HasForeignKey(part => part.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+
+            // Event -> Invitation (Cascade delete: Event silinince Invitation'lar da silinir)
+            modelBuilder.Entity<Event>()
+                .HasMany(evt => evt.Invitations)
+                .WithOne(inv => inv.Event)
+                .HasForeignKey(inv => inv.EventId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Event -> Participant (Cascade delete: Event silinince Participant kayıtları da silinir)
+            modelBuilder.Entity<Event>()
+                .HasMany(evt => evt.Participants)
+                .WithOne(part => part.Event)
+                .HasForeignKey(part => part.EventId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Invitation -> User (Receiver olarak, User silinirse Invitation silinir)
             modelBuilder.Entity<Invitation>()
-           .HasOne(i => i.Event)
-           .WithMany(e => e.Invitations)
-           .HasForeignKey(i => i.EventId)
-           .OnDelete(DeleteBehavior.Cascade);
-
- 
-
-            modelBuilder.Entity<Participant>()
-             .HasOne(p => p.Event)
-             .WithMany(e => e.Participants)
-             .HasForeignKey(p => p.EventId)
-             .OnDelete(DeleteBehavior.Cascade);
-
-
-
-            modelBuilder.Entity<Invitation>()
-              .HasOne(i => i.Organizer)
-              .WithMany(u => u.SentInvitations)
-              .HasForeignKey(i => i.OrganizerId)
-              .OnDelete(DeleteBehavior.NoAction);
-
+                .HasOne(inv => inv.Receiver)
+                .WithMany(user => user.ReceivedInvitations)
+                .HasForeignKey(inv => inv.ReceiverId)
+                .OnDelete(DeleteBehavior.NoAction);
 
 
         }
