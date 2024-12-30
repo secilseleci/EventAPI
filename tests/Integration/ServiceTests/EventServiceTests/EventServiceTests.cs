8336f897d830e7f98d501c4624574be60894e56f
Seçil Seleci <143125124+secilseleci@users.noreply.github.com>
2024-12-30 17:21:48 +0300

    DeleteEventAsync Tests are done

--- tests/Integration/ServiceTests/EventServiceTests/EventServiceTests.cs
+++ tests/Integration/ServiceTests/EventServiceTests/EventServiceTests.cs
@@ -226,11 +226,111 @@ namespace Integration.ServiceTests.EventServiceTests
 
         }
 
-        
+
         #endregion
 
         #region DeleteEvent
+        [Fact]
+        public async Task DeleteEventAsync_Should_Return_EventNotFound_When_Event_Does_Not_Exist()
+        {
+            //Arrange
+            var validUserId = Guid.NewGuid();
+            var eventId = Guid.NewGuid();
 
+            _eventRepositoryMock.Setup(e => e.GetByIdAsync(eventId))
+            .ReturnsAsync((Event)null);
+
+            //Act
+            var result=await _eventService.DeleteEventAsync(eventId, validUserId, CancellationToken.None);
+            //Assert
+            Assert.IsType<ErrorResult>(result);
+            Assert.Equal(Messages.EventNotFound, result.Message);
+        }
+
+        [Fact]
+        public async Task DeleteEventAsync_Should_Return_UnauthorizedAccess_When_User_Is_Not_Organizer()
+        {
+            //Arrange
+            var validUserId = Guid.NewGuid();
+            var eventId = Guid.NewGuid();
+
+            var eventEntity = new Event
+            {
+                Id = eventId,
+                OrganizerId = Guid.NewGuid(),
+                EventName = "Test Event",
+                Location = "Test location",
+                EndDate = DateTimeOffset.UtcNow.AddHours(2),
+                StartDate = DateTimeOffset.UtcNow,
+                EventDescription = "A description for the test event",
+                Timezone = "UTC",
+            };
+            _eventRepositoryMock.Setup(e => e.GetByIdAsync(eventId))
+            .ReturnsAsync(eventEntity);
+
+            //Act
+            var result = await _eventService.DeleteEventAsync(eventId, validUserId, CancellationToken.None);
+            //Assert
+            Assert.IsType<ErrorResult>(result);
+            Assert.Equal(Messages.UnauthorizedAccess, result.Message);
+        }
+
+        [Fact]
+        public async Task DeleteEventAsync_Should_Return_ErrorResult_When_DatabaseRemove_Fails()
+        {
+            //Arrange
+            var validUserId = Guid.NewGuid();
+            var eventId = Guid.NewGuid();
+            var eventEntity = new Event
+            {
+                Id = eventId,
+                OrganizerId = validUserId,
+                EventName = "Test Event",
+                Location = "Test location",
+                EndDate = DateTimeOffset.UtcNow.AddHours(2),
+                StartDate = DateTimeOffset.UtcNow,
+                EventDescription = "A description for the test event",
+                Timezone = "UTC",
+            };
+            _eventRepositoryMock.Setup(e => e.GetByIdAsync(eventId))
+            .ReturnsAsync(eventEntity);
+            _eventRepositoryMock.Setup(r => r.DeleteAsync(It.IsAny<Guid>()))
+            .ReturnsAsync(0);
+            //Act
+            var result = await _eventService.DeleteEventAsync(eventId, validUserId, CancellationToken.None);
+            //Assert
+            Assert.IsType<ErrorResult>(result);
+            Assert.Equal(Messages.DeleteEventError, result.Message);
+        }
+
+        [Fact]
+        public async Task DeleteEventAsync_Should_Return_SuccessResult_When_All_Conditions_Are_Met()
+        {
+            //Arrange
+            var validUserId = Guid.NewGuid();
+            var eventId = Guid.NewGuid();
+            var eventEntity = new Event
+            {
+                Id = eventId,
+                OrganizerId = validUserId,
+                EventName = "Test Event",
+                Location = "Test location",
+                EndDate = DateTimeOffset.UtcNow.AddHours(2),
+                StartDate = DateTimeOffset.UtcNow,
+                EventDescription = "A description for the test event",
+                Timezone = "UTC",
+            };
+            _eventRepositoryMock.Setup(e => e.GetByIdAsync(eventId))
+            .ReturnsAsync(eventEntity);
+            _eventRepositoryMock.Setup(r => r.DeleteAsync(It.IsAny<Guid>()))
+            .ReturnsAsync(1);
+            //Act
+            var result = await _eventService.DeleteEventAsync(eventId, validUserId, CancellationToken.None);
+            //Assert
+            Assert.IsType<SuccessResult>(result);
+            Assert.Equal(Messages.DeleteEventSuccess, result.Message);
+
+        }
 
         #endregion
     }
